6da628490bb5f02a786fd0321f8db01bb959dec6
Zhixuan Lai <zhxnlai@gmail.com>
2015-05-08 16:41:29 -0700

    done refactoring

---
+++ EmoticonKeyboard/EmoticonKeyboard/EKEmoticonsCollectionViewController.swift
@@ -0,0 +1,52 @@
+//
+//  EKCategoryCollectionViewController.swift
+//  EmoticonKeyboard
+//
+//  Created by Zhixuan Lai on 5/7/15.
+//  Copyright (c) 2015 Zhixuan Lai. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class EKEmoticonsCollectionViewController: UICollectionViewController {
+
+    var category: Category? {
+        didSet {
+            dataSourceDelegate.category = category
+            collectionView?.reloadData()
+        }
+    }
+    
+    var dataSourceDelegate = EKEmoticonsCollectionViewDataSourceDelegate()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = category!.name
+
+        // Register cell classes
+        self.collectionView!.registerClass(EKEmoticonCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        dataSourceDelegate.category = category
+        dataSourceDelegate.didSelectEmoticon = {emoticon in
+            let text = emoticon.value
+            var pasteboard = UIPasteboard.generalPasteboard()
+            pasteboard.string = text
+        }
+        
+        collectionView?.backgroundColor = UIColor.whiteColor()
+        collectionView?.dataSource = dataSourceDelegate
+        collectionView?.delegate = dataSourceDelegate
+        
+        // Do any additional setup after loading the view.
+        collectionView?.reloadData()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
